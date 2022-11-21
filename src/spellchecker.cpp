734246ebc4ce8666144046aa88bcb19f48e0e65d
a01754574 <a01754574@tec.mx>
2022-11-21 13:52:13 -0600

    Save Git

--- src/spellchecker.cpp
+++ src/spellchecker.cpp
@@ -33,10 +33,26 @@ int main() {
 }
 
 bool read_words(
-        const std::string input_file_name,
-        std::vector<word>& words) {
-    
-
-
-
+        const string input_file_name,
+        vector<word>& words) {
+    ifstream input_file(input_file_name);
+    if (input_file.fail()) {
+        return false;
+    }
+    regex reg_exp("[a-zA-Z]+");
+    smatch match;
+    string text;
+    int line = 0;
+    int column = 0;
+    while (getline(input_file, text)) {
+        line++;
+        column = 1;
+        while (regex_search(text, match, reg_exp)) {
+            column += match.position();
+            words.push_back({match.str(), line, column});
+            column += match.length();
+            text = match.suffix().str();
+        }
+    }
+    return true;
 }
--- src/spellchecker.h
+++ src/spellchecker.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <bits/stdc++.h>
+#include <cctype>
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
@@ -9,15 +11,21 @@ using namespace std;
 
 const int len_soundex = 7;
 
-static unordered_map<char, char> parse_values = {
+unordered_map<char, char> parse_values = {
         {'b', '1'}, {'f', '1'}, {'p', '1'}, {'v', '1'},
         {'c', '2'}, {'g', '2'}, {'j', '2'}, {'k', '2'}, {'q', '2'}, {'s', '2'}, {'x', '2'}, {'z', '2'},
         {'d', '3'}, {'t', '3'},
         {'l', '4'},
         {'m', '5'}, {'n', '5'},
-        {'r', '6'}
+        {'r', '6'},
+        {'a', '.'}, {'e', '.'}, {'i', '.'}, {'o', '.'}, {'u', '.'}, {'h', '.'}, {'w', '.'}, {'y', '.'}
 };
 
-auto soundex(const string& s) {
+auto soundex(const string& token) {
+    token = transform(token.begin(), token.end(), token.begin(), ::toupper);
+    soundex = "";
+
+    soundex += token[0];
+
     
 }
